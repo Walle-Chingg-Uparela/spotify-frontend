15e1c999bdb425c5e320f4828f09a718aa88f1ce
Walter Chingg <namorchingg@gmail.com>
2026-04-25 16:46:03 -0500

    conexion api publica

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://spotify-api-7yzn.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
